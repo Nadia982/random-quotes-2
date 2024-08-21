e2a8f43843ee7b0d077a9347b06fd563d36d4ecd
Natalie Gillam <ngillam@hotmail.co.uk>
2024-08-21 15:22:50 +0100

    removed image attribute from each item in the quotes array and designated author image using an if statement to avoid code repetition

--- script.js
+++ script.js
@@ -42,119 +42,113 @@ const quotes = [
   {
     author: "Shakespeare",
     text: '"This above all: to thine own self be true"',
-    image: "./images/shakespeare.png",
   },
   {
     author: "Shakespeare",
-    text:
-      '"Our doubts are traitors, And make us lose the good we oft might win, By fearing to attempt"',
-    image: "./images/shakespeare.png",
+    text: '"Our doubts are traitors, And make us lose the good we oft might win, By fearing to attempt"',
   },
   {
     author: "Shakespeare",
     text: '"We know what we are, but know not what we may be."',
-    image: "./images/shakespeare.png",
   },
   {
     author: "Shakespeare",
-    text:
-      '"Be not afraid of greatness: some are born great, some achieve greatness and some have greatness thrust upon them."',
-    image: "./images/shakespeare.png",
+    text: '"Be not afraid of greatness: some are born great, some achieve greatness and some have greatness thrust upon them."',
   },
   {
     author: "Aristotle",
     text: '"Education is bitter, but its fruit is sweet."',
-    image: "./images/aristotle.png",
   },
   {
     author: "Aristotle",
     text: '"Pleasure in the job puts perfection in the work."',
-    image: "./images/aristotle.png",
   },
   {
     author: "Aristotle",
     text: '"Good habits formed at youth make all the difference"',
-    image: "./images/aristotle.png",
   },
   {
     author: "Aristotle",
     text: '"Well begun is half done"',
-    image: "./images/aristotle.png",
   },
   {
     author: "Plato",
-    text:
-      '"Do not train a child to learn by force or harshness; but direct them to it by what amuses their minds, so that you may be better able to discover with accuracy the peculiar bent of the genius of each."',
-    image: "./images/plato.png",
+    text: '"Do not train a child to learn by force or harshness; but direct them to it by what amuses their minds, so that you may be better able to discover with accuracy the peculiar bent of the genius of each."',
   },
   {
     author: "Plato",
     text: '"Necessity is the mother of invention."',
-    image: "./images/plato.png",
   },
   {
     author: "Plato",
     text: '"Beauty lies in the eyes of the beholder"',
-    image: "./images/plato.png",
   },
   {
     author: "Plato",
     text: '"Character is simply habit long continued"',
-    image: "./images/plato.png",
   },
   {
     author: "Socrates",
     text: '"The only true wisdom is in knowing that you know nothing"',
-    image: "./images/socrates.png",
   },
   {
     author: "Socrates",
-    text:
-      '"True wisdom comes to each of us when we realise how little we understand about life, ourselves, and the world around us."',
-    image: "./images/socrates.png",
+    text: '"True wisdom comes to each of us when we realise how little we understand about life, ourselves, and the world around us."',
   },
   {
     author: "Socrates",
     text: '"Wisdom begins in wonder"',
-    image: "./images/socrates.png",
   },
   {
     author: "Socrates",
-    text:
-      '"Employ your time in improving yourself by other men\'s writings, so that you shall gain easily what others have laboured hard for."',
-    image: "./images/socrates.png",
+    text: '"Employ your time in improving yourself by other men\'s writings, so that you shall gain easily what others have laboured hard for."',
   },
   {
     author: "Socrates",
     text: '"Knowledge will make you be free."',
-    image: "./images/socrates.png",
   },
 ];
 let quoteIndices = [...Array(quotes.length).keys()];
 
 const getQuote = () => {
   quoteIndices = quoteIndices.sort((a, b) => 0.5 - Math.random());
-  const quote = quotes[quoteIndices[quoteIndices.length - 1]]
+  const quote = quotes[quoteIndices[quoteIndices.length - 1]];
   const quoteText = quote.text;
   quoteContent.innerHTML = quoteText;
   const quoteAuthor = quote.author;
   authorName.innerHTML = quoteAuthor;
-  authorPicture.src = quote.image;
-  
+  if (quoteAuthor === "Plato") {
+    authorPicture.src =
+      "https://assets.codepen.io/6669924/plato.png?format=auto";
+  } else if (quoteAuthor === "Shakespeare") {
+    authorPicture.src =
+      "https://assets.codepen.io/6669924/shakespeare.png?format=auto";
+  } else if (quoteAuthor === "Aristotle") {
+    authorPicture.src =
+      "https://assets.codepen.io/6669924/aristotle.png?format=auto";
+  } else {
+    authorPicture.src =
+      "https://assets.codepen.io/6669924/socrates.png?format=auto";
+  }
+
   const readQuote = (text) => {
     const speech = new SpeechSynthesisUtterance(text);
-    if (quoteAuthor === "Plato"){
-    speech.voice = voices.find(
-      (voice) => voice.name === "Microsoft David - English (United States)"
-    );} else if (quoteAuthor === "Shakespeare"){
+    if (quoteAuthor === "Plato") {
       speech.voice = voices.find(
-      (voice) => voice.name === "Google UK English Male")
-    } else if (quoteAuthor === "Aristotle"){
+        (voice) => voice.name === "Microsoft David - English (United States)"
+      );
+    } else if (quoteAuthor === "Shakespeare") {
       speech.voice = voices.find(
-        (voice) => voice.name === "Microsoft Mark - English (United States)")
+        (voice) => voice.name === "Google UK English Male"
+      );
+    } else if (quoteAuthor === "Aristotle") {
+      speech.voice = voices.find(
+        (voice) => voice.name === "Microsoft Mark - English (United States)"
+      );
     } else {
       speech.voice = voices.find(
-        (voice) => voice.name === "Microsoft George - English (United Kingdom)")
+        (voice) => voice.name === "Microsoft George - English (United Kingdom)"
+      );
     }
     speech.pitch = 0.7;
     speech.volume = 0.5;
@@ -164,14 +158,12 @@ const getQuote = () => {
 
   readQuote(quoteText);
   quoteIndices.pop();
-  
 };
 
 getQuote();
 
-
 const handleNext = () => {
-  synth.cancel()
+  synth.cancel();
   quotesFinishedMessage.innerText = "";
   document.querySelector("blockquote").style.backgroundColor = getRandomColor();
   getQuote();
@@ -191,7 +183,7 @@ document.addEventListener("keydown", (e) => {
     handleNext();
     // The code below is to prevent the code from going forward two quotes when the user uses the keyboard to focus on the "new quote button" then presses the spacebar.
     // Without the code below, the browser would handleNext twice with a single press of the spacebar (once because the spacebar has been pressed, and once because pressing spacebar or enter while the "new quote" button is in focus will also forward to the next quote)
-    // I wanted to keep the option to forward to the next quote by tabbing to the "new quote" button and pressing "enter" or "spacebar" as this is an expected functionality for accessibility (for keyboard users and those who can't use a mouse).  
+    // I wanted to keep the option to forward to the next quote by tabbing to the "new quote" button and pressing "enter" or "spacebar" as this is an expected functionality for accessibility (for keyboard users and those who can't use a mouse).
     newQuoteBtn.disabled = true;
     setTimeout(() => (newQuoteBtn.disabled = false), 200);
   }
